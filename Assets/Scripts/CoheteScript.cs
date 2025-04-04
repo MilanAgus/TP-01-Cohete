4c023981f35e3a985a02494dd7d75d8eb453edb5
MilanAgus <agustina.sofia.gallo@gmail.com>
2025-04-04 10:27:22 -0300

    cosas

--- Assets/Scripts/CoheteScript.cs
+++ Assets/Scripts/CoheteScript.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoheteScript : MonoBehaviour
+public class CoheteSpeed : MonoBehaviour
 {
-    public float coheteSpeed = 0;
+    public float cohete = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +15,6 @@ public class CoheteScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0,coheteSpeed,0);
-    }
-
-    public void Despegue()
-    {
-        coheteSpeed = 0.1f;
+        
     }
 }
